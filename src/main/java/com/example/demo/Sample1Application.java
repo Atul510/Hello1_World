1fab82ed1e8d7ed4482eacd47203acd607644bff
Atul Gautam <a234gautamg@gmail.com>
2023-09-18 14:12:48 +0530

    First Commit

---
+++ src/main/java/com/example/demo/Sample1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Sample1Application {
+	
+	@GetMapping
+	public String HelloWorld() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample1Application.class, args);
+	}
+
+}
